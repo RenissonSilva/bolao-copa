532fb61361007b281a97e2f2bca4fb71a7e0dcdd
Renisson Silva <renissonx@gmail.com>
2023-04-03 21:41:15 -0300

    Criando primeira rota com fastify, fazendo primeira chamada com prisma, gerando tabela de Pool e instalando pacotes para gerar modelo ER e evitar problema de cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,27 @@
 import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+    log: ['query']
+})
 
 async function bootstrap() {
     const fastify = Fastify({
         logger: true,
     })
 
-    fastify.get('/pools/count', () => {
-        return { count: 0 }
+    await fastify.register(cors, {
+        origin: true //Quando for pra produção é interessante alterar para a url da produção. Ex: www.rocketseat.com.br
     })
 
-    await fastify.listen({
-        port: 3333
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count();
+
+        return { count }
     })
+
+    await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 bootstrap()
