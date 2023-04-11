5656d5f87ac96fee0143a7624ede49f9770bc818
Renisson Silva <renissonx@gmail.com>
2023-04-11 19:57:28 -0300

    Criando e organizando arquivos de rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,11 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors'
-import { z } from 'zod'
-import { PrismaClient } from '@prisma/client'
-import ShortUniqueId from 'short-unique-id'
 
-const prisma = new PrismaClient({
-    log: ['query']
-})
+import { poolRoutes } from "./routes/poll";
+import { userRoutes } from "./routes/user";
+import { guessRoutes } from "./routes/guess";
+import { authRoutes } from "./routes/auth";
+import { gameRoutes } from "./routes/game";
 
 async function bootstrap() {
     const fastify = Fastify({
@@ -17,43 +16,11 @@ async function bootstrap() {
         origin: true //Quando for pra produção é interessante alterar para a url da produção. Ex: www.rocketseat.com.br
     })
 
-    fastify.get('/polls/count', async () => {
-        const count = await prisma.poll.count();
-
-        return { count }
-    })
-
-    fastify.get('/users/count', async () => {
-        const count = await prisma.user.count();
-
-        return { count }
-    })
-
-    fastify.get('/guesses/count', async () => {
-        const count = await prisma.guess.count();
-
-        return { count }
-    })
-
-    fastify.post('/polls', async (request, reply) => {
-        const createPollBody = z.object({
-            title: z.string()
-        })
-
-        const { title } = createPollBody.parse(request.body)
-
-        const generate = new ShortUniqueId({ length: 6 })
-        const code = String(generate()).toUpperCase()
-
-        await prisma.poll.create({
-            data: {
-                title,
-                code
-            }
-        })
-
-        return reply.status(201).send({ code })
-    })
+    await fastify.register(poolRoutes)
+    await fastify.register(userRoutes)
+    await fastify.register(guessRoutes)
+    await fastify.register(authRoutes)
+    await fastify.register(gameRoutes)
 
     await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ })
 }
