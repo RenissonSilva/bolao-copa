f3a5c86f882d2f034641def78fe32e48650f2748
Renisson Silva <renissonx@gmail.com>
2023-04-15 01:18:27 -0300

    Ajustando rota da api

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const api = axios.create({
     // Para ver seu endereço IP, basta olhar no expo, ao executar ele é mostrado
-    baseURL: 'http://10.0.2.2:3333/api'
+    baseURL: 'http://10.0.2.2:3333'
 })
